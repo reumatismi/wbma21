b83d3c237a76aaeab67ddcb02c8225e512b1e3e0
Matti Stenvall <matti.stenvall@metropolia.fi>
2021-08-24 14:27:31 +0300

    Task C done (this time for realz)

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -1,27 +1,41 @@
 import {useEffect, useState} from 'react';
 import {baseUrl} from '../utils/variables';
+import {doFetch} from '../utils/http';
 
 const useMedia = () => {
   const [mediaArray, setMediaArray] = useState([]);
   useEffect(() => {
-    const loadMedia = async () => {
-      try {
-        const response = await fetch(baseUrl + 'media');
-        const mediaIlmanThumbNailia = await response.json();
-        const kaikkiTiedot = mediaIlmanThumbNailia.map(async (media) => {
-          const response = await fetch(baseUrl + 'media/' + media.file_id);
-          const tiedosto = await response.json();
-          return tiedosto;
-        });
-        setMediaArray(await Promise.all(kaikkiTiedot));
-      } catch (e) {
-        console.log('List.js - List: ', e.message);
-      }
-    };
-    loadMedia();
+    (async () => {
+      setMediaArray(await loadMedia());
+    })(); // self-invoking function CHECK CHECK
   }, []);
 
-  return {mediaArray};
+  const loadMedia = async () => {
+    try {
+      const mediaIlmanThumbNailia = await doFetch(baseUrl + 'media');
+      const kaikkiTiedot = mediaIlmanThumbNailia.map(async (media) => {
+        try {
+          return await loadSingleMedia(media.file_id);
+        } catch (e) {
+          return {};
+        }
+      });
+      return Promise.all(kaikkiTiedot);
+    } catch (e) {
+      console.log('List.js - List: ', e.message());
+    }
+  };
+
+  const loadSingleMedia = async (id) => {
+    try {
+      const tiedosto = await doFetch(baseUrl + 'media/' + id);
+      return tiedosto;
+    } catch (e) {
+      console.log('loadMedia' + e.message);
+    }
+  };
+
+  return {mediaArray, loadSingleMedia, loadMedia};
 };
 
 export {useMedia};
